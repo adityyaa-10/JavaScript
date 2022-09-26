12d5f5947646235ab909f8309f501b9a29d8b325
Aditya Pandey <aditya.pandey.241103@gmail.com>
2022-09-27 00:23:56 +0530

    First Commit

---
+++ variables.js
@@ -0,0 +1,26 @@
+// // alert('Hello World');
+// console.log(1);
+// console.log(2);
+
+//Let Keyword
+let age = 19;
+let year = 2003;
+
+console.log(age);
+console.log(year);
+console.log(age,year);
+
+//Let keyword is updatable. eg=>
+age = 18;
+console.log(age);
+
+//const keyword;
+
+const cents = 100;
+console.log(cents);
+
+//Now if we try to re write a const type variable, an error will occur;
+cents = 500;
+console.log(cents);         //Uncaught TypeError: Assignment to constant variable.
+
+
