045227f42bf1a15c91867f964b0c81f77c74387a
Aditya <aditya.pandey.241103@gmail.com>
2022-10-26 22:50:58 +0530

    Added DOM 1 2 & 3

--- Document Object Model/DOM2/scripts.js
+++ Document Object Model/DOM2/scripts.js
@@ -7,3 +7,10 @@ console.log(Array.from(article.children));//To cinvert HTML Collection into an A
 Array.from(article.children).forEach((child) => {
     child.classList.add('article-element'); //To add a new class to every child of a parent in JS
 })
+
+const title = document.querySelector('h2');
+console.log(title.parentElement);
+console.log(title.parentElement.parentElement);
+
+console.log(title.nextElementSibling);
+console.log(title.previousElementSibling);
