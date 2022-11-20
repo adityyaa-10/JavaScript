3626e81acf8a75779ca3ec4f21e419577053477f
Aditya <aditya.pandey.241103@gmail.com>
2022-11-20 21:05:50 +0530

    Form Validation

--- Forms and Form Events/submit.js
+++ Forms and Form Events/submit.js
@@ -1,8 +1,28 @@
 const form = document.querySelector('.signup-form');
-const username = document.querySelector('#username');
+// const username = document.querySelector('#username');
 
 form.addEventListener('submit', e =>{
 e.preventDefault();
 // console.log(username.value);
 console.log(form.username.value);
 });
+
+const username =  'adityyaa';
+
+const pattern = /^[a-z]{6,}$/;
+
+// let result = pattern.test(username);
+// console.log(result);
+
+// if(result){
+//     console.log('passed');
+
+// }
+
+// else{
+//     console.log('failed')
+// }
+
+let result = username.search(pattern);
+
+console.log(result);
